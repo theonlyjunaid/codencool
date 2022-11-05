cf9554d6bc33c28dcfe74023c41302e4a0ad5b77
junaid <onlyjunaid2002@gmail.com>
2022-11-05 23:56:41 +0530

    gdf

--- components/home/Navbar.jsx
+++ components/home/Navbar.jsx
@@ -5,14 +5,14 @@ const Navbar = ({heading}) => {
     <div  className=''>
 
       <div className='flex justify-between bg-white w-3/4 m-auto rounded-2xl'>
-        <span className='text-3xl p-3 m-auto'>{heading}</span>
+        <span className='text-3xl p-3 m-auto'>hola</span>
         <span className='flex w-3/4 justify-evenly ml-40 my-4'>
           <span className='text-2xl font-light p-3 border-t-4 border-pink-500'>About</span>
           <span className='text-2xl font-light p-3'>Speakers</span>
           <span className='text-2xl font-light p-3'>Partners</span>
           <span className='text-2xl font-light p-3'>FAQs</span>
           <span className='text-2xl font-light p-3'>Venue</span>
-          <button className='text-2xl p-3 bg-pink-500 rounded-2xl px-5 text-white'>Apply Now</button>
+          <button className='text-2xl p-3 bg-pink-500 rounded-2xl px-5 text-white'>Login in</button>
         </span>
       </div>
     </div>
