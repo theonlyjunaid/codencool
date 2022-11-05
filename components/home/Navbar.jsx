cf6358adef927a019892c3800474a1f4214b99f6
junaid <onlyjunaid2002@gmail.com>
2022-11-06 00:03:34 +0530

    gdfsz

--- components/home/Navbar.jsx
+++ components/home/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = ({heading}) => {
   return (
     <div  className=''>
 
       <div className='flex justify-between bg-white w-3/4 m-auto rounded-2xl'>
-        <span className='text-3xl p-3 m-auto'>hola</span>
+        <div className='flex justify-center items-center'><Link href="/"><span className='text-3xl ml-8'>hola</span></Link></div>
         <span className='flex w-3/4 justify-evenly ml-40 my-4'>
           <span className='text-2xl font-light p-3 border-t-4 border-pink-500'>About</span>
           <span className='text-2xl font-light p-3'>Speakers</span>
           <span className='text-2xl font-light p-3'>Partners</span>
           <span className='text-2xl font-light p-3'>FAQs</span>
           <span className='text-2xl font-light p-3'>Venue</span>
-          <button className='text-2xl p-3 bg-pink-500 rounded-2xl px-5 text-white'>Login in</button>
+          <Link href="/login"><button className='text-2xl p-3 bg-pink-500 rounded-2xl px-5 text-white'>Login in</button></Link>   
         </span>
       </div>
     </div>
