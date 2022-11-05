e7bb49afbea3de154af0faff5b4a75062e3a8192
Ashish Jha <88213707+ashjha03@users.noreply.github.com>
2022-11-06 01:27:28 +0530

    About Section Added

---
+++ components/home/About.jsx
@@ -0,0 +1,31 @@
+import Navbar from "./Navbar"
+
+function about() {
+  return (
+    <div>
+        <section id="about" className="text-gray-600 bg-white body-font">
+            <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+                <h1 className="text-5xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit</h1>
+                </div>
+                <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                    Knausgaard typewriter readymade marfa
+                </h1>
+                <p className="mb-8 leading-relaxed">
+                    Chillwave portland ugh, knausgaard fam polaroid iPhone. Man braid swag
+                    typewriter affogato, hella selvage wolf narwhal dreamcatcher.
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel, quibusdam dicta ipsam consectetur odit vitae modi, soluta impedit tempora, sed pariatur magnam distinctio eos similique ea ipsum eius quos minima. Officia, modi. Cum numquam sint nam soluta tempore odit dolores sed amet itaque? Dolorem iure nostrum corporis, sunt culpa soluta vel temporibus modi ab fugiat facilis totam!
+                    <br /> Numquam reiciendis vitae repellendus rerum. Odit eos, esse quo reiciendis iure laborum rem iusto odio porro atque voluptatum a earum tempore officiis excepturi qui ullam aliquid veritatis nisi, explicabo libero ipsam. Totam asperiores nemo illo hic aspernatur, deleniti error, debitis optio doloremque culpa cumque accusamus iure ducimus sequi dolores nobis, minus magni repellat quod consequuntur et dignissimos eligendi placeat rem? Aliquid, fuga. Possimus, ex natus. Nobis, quisquam eaque. Quibusdam fuga illo doloribus ad veritatis magni natus hic unde nulla, in sed minima. Voluptatum dolores voluptas totam laboriosam at ipsum enim ipsam voluptates magnam.
+                </p>
+                
+                </div>
+            </div>
+            </section>
+
+
+    </div>
+  )
+}
+
+export default about
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Center from '../components/home/Center'
 import Footer from '../components/home/Footer'
 import Navbar from '../components/home/Navbar'
+import About from '../components/home/About'
 
 
 const index = () => {
@@ -10,6 +11,8 @@ const index = () => {
       <Navbar/>
   
       <Center/>
+
+      <About/>
 
       <Footer/>
     </div>
