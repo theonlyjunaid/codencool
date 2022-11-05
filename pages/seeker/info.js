33f5a3bd546d9fcc1a98dd504cae53b5d85464a8
junaid <onlyjunaid2002@gmail.com>
2022-11-06 03:40:47 +0530

    Gdf

--- pages/seeker/info.js
+++ pages/seeker/info.js
@@ -6,9 +6,8 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 function Info() {
-  const data = async () => {
-    
-  }
+  
+  
 const [name, setName] = useState('')
 const [email, setEmail] = useState('')
   const [Category, setCategory] = useState('')
@@ -60,7 +59,7 @@ const [email, setEmail] = useState('')
       body: JSON.stringify(data)
     })
     const json = await res.json()
-console.log(json)
+// console.log(json)
 
 
 
