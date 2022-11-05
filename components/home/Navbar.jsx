0711c8dc28776b24b5f3a009ae82b640ced0d8ae
Ashish Jha <88213707+ashjha03@users.noreply.github.com>
2022-11-06 04:12:33 +0530

    Main page updated

--- components/home/Navbar.jsx
+++ components/home/Navbar.jsx
@@ -3,15 +3,15 @@ import Link from 'next/link'
 
 const Navbar = ({heading}) => {
   return (
-    <div  className='sticky top-2 z-20'>
+    <div  className='sticky top-2 z-20 opacity-70'>
       <div className='flex justify-between navbarshadow bg-cyan-50 w-4/5 m-auto rounded-2xl'>
-        <div className='flex justify-center items-center'><Link href="/"><span className='text-3xl ml-8'>hola</span></Link></div>
+        <div className='flex justify-center items-center'><Link href="/"><span className='text-3xl ml-8'>ReferVilla</span></Link></div>
         <span className='flex w-3/4 justify-evenly ml-40 my-4'>
           {/* border-t-4 border-pink-500 */}
-          <Link href="#about"><span className='text-2xl font-light p-3 '>About</span></Link>
-          <Link href="#"><span className='text-2xl font-light p-3'>Resources</span></Link>
-          <Link href="#"><span className='text-2xl font-light p-3'>Partners</span></Link>
-          <Link href="#"><span className='text-2xl font-light p-3'>FAQs</span></Link>
+          <Link href="#about" className='p-3'><span className='text-2xl font-light'>About</span></Link>
+          <Link href="#" className='p-3'><span className='text-2xl font-light'>Resources</span></Link>
+          <Link href="#" className='p-3'><span className='text-2xl font-light'>Partners</span></Link>
+          <Link href="#" className='p-3'><span className='text-2xl font-light'>FAQs</span></Link>
 
           <Link href="/login"><button className='text-2xl p-3 bg-pink-500 rounded-2xl px-5 text-white'>Login</button></Link>   
         </span>
