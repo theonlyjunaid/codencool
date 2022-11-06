099ffacb80cd883525b2d1deb1d0e3e673996d7f
kunal00000 <kunalverma2468@gmail.com>
2022-11-06 11:08:53 +0530

    kunal

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,15 @@ import Navbar from '../components/home/Navbar'
 import About from '../components/home/About'
 import Landing from '../components/home/Landing'
 import Faq from '../components/home/Faq'
+import Second from '../components/home/Second'
 
 const index = () => {
   return (
     <div className="">
       <Navbar/>
-
       <Landing/>
       <About/>
+      <Second />
       <Faq/>
       <Footer/>
     </div>
