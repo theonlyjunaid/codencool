c556c857c3074676c220ec20a44380c5e94cf1f3
Ashish Jha <88213707+ashjha03@users.noreply.github.com>
2022-11-06 09:23:01 +0530

    faqs added

--- components/home/About.jsx
+++ components/home/About.jsx
@@ -6,11 +6,12 @@ function about() {
         <section id="about" className="text-gray-600 bg-white body-font">
             <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
                 <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-                <h1 className="text-5xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit</h1>
+                <h1 className="text-6xl">ReferVilla !</h1> 
+                <h1 className="text-5xl pt-3">  &nbsp;&nbsp;&nbsp;&nbsp; आपको मिला?</h1>
                 </div>
                 <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                 <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-                    Knausgaard typewriter readymade marfa
+                    ReferVilla : : The world of referrals
                 </h1>
                 <p className="mb-8 leading-relaxed">
                     Chillwave portland ugh, knausgaard fam polaroid iPhone. Man braid swag
--- components/home/Navbar.jsx
+++ components/home/Navbar.jsx
@@ -3,15 +3,15 @@ import Link from 'next/link'
 
 const Navbar = ({heading}) => {
   return (
-    <div  className='sticky top-2 z-20 opacity-70'>
-      <div className='flex justify-between navbarshadow bg-cyan-50 w-4/5 m-auto rounded-2xl'>
+    <div  className='fixed w-[100%] top-2 z-20 opacity-90'>
+      <div className='flex justify-between navbarshadow bg-green-100 w-4/5 m-auto rounded-2xl'>
         <div className='flex justify-center items-center'><Link href="/"><span className='text-3xl ml-8'>ReferVilla</span></Link></div>
         <span className='flex w-3/4 justify-evenly ml-40 my-4'>
           {/* border-t-4 border-pink-500 */}
           <Link href="#about" className='p-3'><span className='text-2xl font-light'>About</span></Link>
           <Link href="#" className='p-3'><span className='text-2xl font-light'>Resources</span></Link>
           <Link href="#" className='p-3'><span className='text-2xl font-light'>Partners</span></Link>
-          <Link href="#" className='p-3'><span className='text-2xl font-light'>FAQs</span></Link>
+          <Link href="#faq" className='p-3'><span className='text-2xl font-light'>FAQs</span></Link>
 
           <Link href="/login"><button className='text-2xl p-3 bg-pink-500 rounded-2xl px-5 text-white'>Login</button></Link>   
         </span>
